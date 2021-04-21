f624c911830e36abe79c1a3768e9b4022d93d4e7
64J0 <vinigaio97@gmail.com>
2021-04-21 18:17:35 -0300

    feat: force table name, set the table name with a string defined by me

--- src/database/models/Tweet.ts
+++ src/database/models/Tweet.ts
@@ -6,6 +6,7 @@ class Tweet extends Model {
       text: DataTypes.STRING,
     }, {
       sequelize: connection,
+      tableName: "tweets",
     });
   }
 
--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -8,6 +8,7 @@ class User extends Model {
       password: DataTypes.STRING,
     }, {
       sequelize: connection,
+      tableName: "users",
     });
   }
 
